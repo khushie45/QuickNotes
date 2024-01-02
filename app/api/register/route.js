a357b6b1358628eb3f0710a8e7064f8aa9fcfedd
Khushi <khushipatel3340@gmail.com>
2024-01-02 19:50:27 +0530

    Mongodb connection

---
+++ app/api/register/route.js
@@ -0,0 +1,20 @@
+import User from "@/models/user";
+import { NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
+
+export const POST = async (req) => {
+  try {
+    const { name, email, password } = await req.json();
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await User.create({ name, email, password: hashedPassword });
+
+    return NextResponse.json({ message: "User registered." }, { status: 201 });
+  } catch (error) {
+    console.error("Error during registration:", error);
+    return NextResponse.json(
+      { message: "An error occured while registering the user." },
+      { status: 500 }
+    );
+  }
+};
--- components/RegisterForm.js
+++ components/RegisterForm.js
@@ -1,36 +1,106 @@
+"use client";
+
 import Link from "next/link";
+import { useState } from "react";
+import { useRouter } from "next/router";
 
 const RegisterForm = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!name || !email || !password) {
+      setError("All fields are necessary.");
+      return;
+    }
+
+    try {
+      const resUserExists = await fetch("api/userExists", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email,
+        }),
+      });
+
+      const { user } = await resUserExists.json();
+
+      if (user) {
+        setError("User already exists.");
+        return;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+
+    try {
+      const res = await fetch("api/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name,
+          email,
+          password,
+        }),
+      });
+
+      if (res.ok) {
+        const form = e.target;
+        form.reset();
+        router.push("/");
+      } else {
+        console.log("User registration failed.");
+      }
+    } catch (error) {
+      console.log("Error during registration", error);
+    }
+  };
+
   return (
     <div className="grid place-items-center h-screen">
       <div className="shadow-lg p-5 rounded-lg border-t-4 border-green-400">
         <h1 className="text-x; font-bold my-4">Register</h1>
-        <form className="flex flex-col gap-3">
-          {/* <label htmlFor="fullName">Full Name</label>  */}
+        <form onSubmit={handleSubmit} className="flex flex-col gap-3">
           <input
             type="text"
             name="fullName"
             id="fullName"
             placeholder="Full Name"
+            onChange={(e) => setName(e.target.value)}
           />
-          {/* <label htmlFor="email">Email</label> */}
-          <input type="email" name="email" id="email" placeholder="Email" />
-
-          {/* <label htmlFor="password">Password</label>  */}
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
           <input
             type="password"
             name="password"
             id="password"
             placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
           />
 
           <button className="bg-green-600 text-white font-bold cursor-pointer px-6 py-2">
             Register
           </button>
 
-          <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">
-            Error Message
-          </div>
+          {error && (
+            <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">
+              {error}
+            </div>
+          )}
 
           <Link className="text-sm mt-3 text-right" href={"/"}>
             Already have an account? <span className="underline">Login</span>
